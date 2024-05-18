f368abf5377acb64fabf34585a73a80c225ee7ee
Vedansh Mishra <vedanshm2001@gmail.com>
2024-05-19 00:22:47 +0530

    added first project

---
+++ html_css_js_practice/background_changer_buttons/main.js
@@ -0,0 +1,27 @@
+document.getElementById('button1').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'red';
+});
+
+document.querySelector('.button2').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'yellow';
+});
+
+document.querySelector('.button3').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'black';
+});
+
+document.querySelector('.button4').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'purple';
+});
+
+document.querySelector('.button5').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'green';
+});
+
+document.querySelector('.button6').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'blue';
+});
+
+document.querySelector('.button7').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'white';
+});
